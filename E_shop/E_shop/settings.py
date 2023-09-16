ab2c1178aa5a47a33f7e508892029ac0f7b59ce9
ashish-dhamne <ashishdhamne27@gmail.com>
2023-09-16 16:58:57 +0530

    vercel added

--- E_shop/E_shop/settings.py
+++ E_shop/E_shop/settings.py
@@ -34,7 +34,7 @@ EMAIL_HOST_USER = 'ashishdhamne27gmail.com'
 EMAIL_HOST_PASSWORD = 'efaowjmaganmhgog' 
 EMAIL_USE_TLS = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
